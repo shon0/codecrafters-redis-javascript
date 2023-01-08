a52c02dd1cb125cc1cc9d8e59b6f1cd8b376db66
shon0 <shono.koga@gmail.com>
2023-01-08 11:46:18 +0900

    pass 2st stage

--- app/main.js
+++ app/main.js
@@ -5,7 +5,9 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", ()=> {
+    connection.write("+PONG\r\n")
+  })
 });
 
 server.listen(6379, "127.0.0.1");
